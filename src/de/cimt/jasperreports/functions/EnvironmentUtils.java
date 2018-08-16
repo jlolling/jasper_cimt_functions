b4f2f6e03680e0725a77b618856e98638ede5eb7
Jan Lolling <jan.lolling@gmail.com>
2018-08-16 22:17:41 +0200

    GET_ENV_VAR method added

--- src/de/cimt/jasperreports/functions/EnvironmentUtils.java
+++ src/de/cimt/jasperreports/functions/EnvironmentUtils.java
@@ -4,6 +4,8 @@ import java.lang.management.ManagementFactory;
 
 import net.sf.jasperreports.functions.annotations.Function;
 import net.sf.jasperreports.functions.annotations.FunctionCategories;
+import net.sf.jasperreports.functions.annotations.FunctionParameter;
+import net.sf.jasperreports.functions.annotations.FunctionParameters;
 
 @FunctionCategories({ de.cimt.jasperreports.functions.EnvironmentUtilsCategory.class })
 public class EnvironmentUtils {
@@ -28,6 +30,16 @@ public class EnvironmentUtils {
 			} else {
 				return info;
 			}
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	@Function("GET_ENV_VAR")
+	@FunctionParameters({ @FunctionParameter("string") })
+	public static String GET_ENV_VAR(String name) {
+		try {
+			return System.getenv(name);
 		} catch (Exception e) {
 			return null;
 		}
